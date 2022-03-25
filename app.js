0d321401decfea81d229eb43bb8ff28a365cb856
Rakesh Gain <rakeshgain47681@gmail.com>
2022-03-25 08:37:43 +0530

    post query added

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 const express = require("express")
 
 const https = require("https")
+const bodyParser = require("body-parser")
 const app = express()
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/",function(req, res){
-    const query = "Bhawanipatna";
-    const apiKey = "c5dc6076313b974f371571d1e2cb6672";
-    const urls = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=metric";
-    https.get(urls, function(response){
-        console.log(response.statusCode);
-        response.on("data",function(data){
-            const weatherData = JSON.parse(data);
-            console.log(weatherData);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
-            
-            console.log(temp);
-            console.log(weatherDescription);
-            res.write("<p> The weather is currently "+weatherDescription+"<img src="+imageUrl+"></p>")
-            res.write("<h1>The temperature in "+query+" is "+temp+" celcious<h1>")
-            res.send()
-        })
-    });
+    res.sendFile(__dirname+"/index.html")
     // res.send("Server is up nad running.");
     
 })
 
 
+app.post("/",function(req, res){
+    
+    const query = req.body.cityname;
+    const apiKey = "c5dc6076313b974f371571d1e2cb6672";
+    const urls = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=metric";
+    https.get(urls, function(response){
+        // console.log(response.statusCode);
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            // console.log(weatherData);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            
+            // console.log(temp);
+            // console.log(weatherDescription);
+            res.write("<p> The weather is currently "+weatherDescription+"</p>")
+            res.write("<h1>The temperature in "+query+" is "+temp+" celcious<h1>")
+            res.write("<img src="+imageUrl+">")
+            res.send()
+        })
+    });
+    
+})
+
+// 
 
 
 app.listen(3000, function(){
